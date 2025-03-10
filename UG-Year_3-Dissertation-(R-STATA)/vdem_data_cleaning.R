7dd153d5d3f4a03bc985889ed6a74a0a65eb306e
Ahan-Thakur <127997009+Ahan-Thakur@users.noreply.github.com>
2025-03-10 07:30:59 +0000

    Update vdem_data_cleaning.R

--- UG-Year_3-Dissertation-(R-STATA)/vdem_data_cleaning.R
+++ UG-Year_3-Dissertation-(R-STATA)/vdem_data_cleaning.R
@@ -1,5 +1,5 @@
-# Load the RData file
 data <- readRDS("C:/Ahan/Ahan/University 3rd Year/Dissertation/Dissertation Code/vdem_data_raw/V-Dem-CY-Full+Others-v14.rds")
+output_dir <- "C:/Ahan/Ahan/University 3rd Year/Dissertation/Dissertation Code/vdem_data"
 
 colnames(data)[colnames(data) == "country_name"] <- "country"
 colnames(data)[colnames(data) == "country_text_id"] <- "countrycode"
@@ -8,7 +8,6 @@ colnames(data)[colnames(data) == "v2x_libdem"] <- "libdem"
 colnames(data)[colnames(data) == "v2x_delibdem"] <- "delibdem"
 colnames(data)[colnames(data) == "v2x_egaldem"] <- "egaldem"
 
-# Select relevant columns
 selected_vars <- c("country", "countrycode", "year", "elecdem", "libdem", "delibdem", "egaldem")
 vdem_data_clean <- data[selected_vars]
 
@@ -19,10 +18,6 @@ vdem_data_clean$institutions <- (vdem_data_clean$elecdem + vdem_data_clean$libde
 required_vars <- c("libdem", "elecdem", "delibdem", "egaldem")
 vdem_data_clean <- vdem_data_clean[complete.cases(vdem_data_clean[, required_vars]), ]
 
-# Define output directory
-output_dir <- "C:/Ahan/Ahan/University 3rd Year/Dissertation/Dissertation Code/vdem_data"
-
-# Save the cleaned data as an RData file
 save(vdem_data_clean, file = file.path(output_dir, "vdem_data_clean.RData"))
 
 # Print message confirming successful processing
